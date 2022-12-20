811bdc9806021d2042c7ec3a6f69981b12deb8cd
Gurgen Yegoryan <gurgen.yegoryan@gmail.com>
2022-12-20 10:24:38 +0400

    Modified

--- main.py
+++ main.py
@@ -13,36 +13,14 @@ file_rfc = ns_path + 'named.rfc1912.zones'
 
 def up_hosts():
     ipaddress_list = []
-
+    print("Start searching for active ip addresses")
     for ip in ipaddress.IPv4Network('192.168.4.0/24'):
         status, result = subprocess.getstatusoutput("timeout 0.2 ping -c1 " + str(ip))
         if status == 0:
             ipaddress_list.append(str(ip))
     ipaddress_list.remove('192.168.4.101')
+    print("Active ip addresses founded")
     return ipaddress_list
-
-
-def get_hostname(user_pass, user_name):
-    addresses = up_hosts()
-    for ip in addresses:
-        print(ip)
-        try:
-            cmd1 = f"sshpass -p {user_pass} ssh -o StrictHostKeyChecking=no {user_name}@{ip} -o LogLevel=ERROR uname -n"
-            cmd2 = "sshpass -p %s ssh -o StrictHostKeyChecking=no %s@%s -o LogLevel=ERROR /sbin/ifconfig tun0 | grep " \
-                   "netmask | awk '{print $2}'" % (
-                       user_pass, user_name, str(ip))
-
-            hostname = subprocess.getoutput(cmd1)
-            vpn_ip = subprocess.getoutput(cmd2)
-
-        except PermissionError as e:
-            print(f"Can't connect to {ip} host: {e}")
-            sys.exit(1)
-
-        finally:
-            create_host_file(hostname, ip, vpn_ip)
-            append_global_zone(hostname)
-            print(f"{hostname} db file created")
 
 
 def create_host_file(hostname, ip, vpn_ip):
@@ -63,6 +41,7 @@ def create_host_file(hostname, ip, vpn_ip):
     hostname_file = f"{ns_path}{hostname}.db"
     with open(hostname_file, "w") as f:
         f.write(host_db_file)
+    print(f"{hostname} db file created")
 
 
 def append_global_zone(hostname):
@@ -75,6 +54,32 @@ def append_global_zone(hostname):
 
     with open(file_rfc, "a") as f:
         f.write(str_rfc)
+
+
+def get_hostname(user_pass, user_name):
+    addresses = up_hosts()
+    for ip in addresses:
+        try:
+            cmd1 = f"sshpass -p {user_pass} ssh -o StrictHostKeyChecking=no {user_name}@{ip} -o LogLevel=ERROR uname -n"
+            cmd2 = "sshpass -p %s ssh -o StrictHostKeyChecking=no %s@%s -o LogLevel=ERROR /sbin/ifconfig tun0 | grep " \
+                   "netmask | awk '{print $2}'" % (
+                       user_pass, user_name, str(ip))
+
+            hostname = subprocess.getoutput(cmd1)
+            vpn_ip = subprocess.getoutput(cmd2)
+            if ' ' in hostname:
+                print(f"Can't connect to host {ip}: {hostname}")
+                continue
+            elif hostname == '':
+                print(f"In {ip} dont set hostname: {hostname}")
+                continue
+            else:
+                create_host_file(hostname, ip, vpn_ip)
+                append_global_zone(hostname)
+
+        except ConnectionAbortedError as e:
+            print(f"Can't connect to {ip} host: {e}")
+            sys.exit(1)
 
 
 get_hostname(us_pass, user)

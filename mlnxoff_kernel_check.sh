904d7e9ca62b4a7de11996639cc2641ee464ebd6
Gurgen Yegoryan <gurgen.yegoryan@gmail.com>
2022-12-26 00:03:56 +0400

    Polish

--- mlnxoff_kernel_check.sh
+++ mlnxoff_kernel_check.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
+# https://network.nvidia.com/products/infiniband-drivers/linux/mlnx_ofed/
 
 # save kernel verion in file and load in variable
-kernel_version_path=$HOME/.config/kernel_version
-kern_version_old=cat $kernel_version_path
-
+kernel_version_path=$HOME/.config/kernel_version.txt
+kern_version=cat $kernel_version_path
 
 # Check if version change, uninstall and install mlnx_ofed and rewrite kernel version file with current version
 if [ uname -r != "$kern_version" ]; then
+    echo "Uninstall last installed Mellanox Drier..."
     cd /tmp &&
-    wget https://content.mellanox.com/ofed/MLNX_OFED-5.8-1.0.1.1/MLNX_OFED_LINUX-5.8-1.0.1.1-ubuntu22.04-x86_64.iso &&
+    wget https://content.mellanox.com/ofed/MLNX_OFED-5.8-1.1.2.1/MLNX_OFED_LINUX-5.8-1.1.2.1-ubuntu22.04-x86_64.iso &&
     mkdir -p mlnx &&
-    mount -o ro,loop MLNX_OFED_LINUX-5.8-1.0.1.1-ubuntu22.04-x86_64.iso ./mlnx &&
+    mount -o ro,loop MLNX_OFED_LINUX-5.8-1.1.2.1-ubuntu22.04-x86_64.iso ./mlnx &&
     ./mlnx/uninstall.sh --force &&
     ./mlnx/mlnxofedinstall --without-dkms --add-kernel-support --kernel $(uname -r) --without-fw-update --force &&
     /etc/init.d/openibd restart

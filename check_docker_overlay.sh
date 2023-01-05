22bfee655a9cc33fe87d30b7244b2f985db1c9df
Gurgen Yegoryan <gurgen.yegoryan@gmail.com>
2023-01-05 18:15:11 +0400

    Creating overlay folder size checking

---
+++ check_docker_overlay.sh
@@ -0,0 +1,23 @@
+# 40 GB
+SIZE="40"
+
+
+# check the current size
+CHECK="`du -sh test/ | cut -f1`"
+echo "Current Foldersize: $CHECK GB"
+
+# Get last character
+last_char= "`echo "${CHECK:0-1}"`"
+
+# Remove last character
+CHECK="`echo ${CHECK:0:-1}`"
+
+if  ["$last_char" = "M" ] || [ "$last_char" = "K" ];then
+    break
+elif (( $(echo "$CHECK > $SIZE" |bc -l) )); then
+    docker system prune -a --volumes
+else
+    break
+fi
+
+

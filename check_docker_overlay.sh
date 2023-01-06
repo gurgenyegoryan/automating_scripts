ed64b12ed5fce5aef12d59d72a9fd9a03df1a77c
Gurgen Yegoryan <gurgen.yegoryan@gmail.com>
2023-01-06 19:01:48 +0400

    Createing new main script, polish

--- check_docker_overlay.sh
+++ check_docker_overlay.sh
@@ -1,23 +1,39 @@
-# 40 GB
-SIZE="40"
+#!/bin/bash
+# Limit for folder 10 GB
+while true
+do
+    LIMIT="10"
 
+    # Overlay2 folder
+    path=/var/lib/docker/overlay2/
 
-# check the current size
-CHECK="`du -sh /var/lib/docker/overlay2/ | cut -f1`"
-echo "Current Foldersize: $CHECK GB"
+    # Check the current size
+    CHECK="`/usr/bin/du -sh $path | /usr/bin/cut -f1`"
 
-# Get last character
-last_char= "`echo "${CHECK:0-1}"`"
+    # Check if folder size 0 or not
+    if [ $CHECK -eq 0 ];then
+        echo "Folder size is 0,nothing to do"
+        /usr/bin/sleep 3600
+        continue
+    fi
 
-# Remove last character
-CHECK="`echo ${CHECK:0:-1}`"
+    echo "Current Foldersize: $CHECK GB"
 
-if  ["$last_char" = "M" ] || [ "$last_char" = "K" ];then
-    break
-elif (( $(echo "$CHECK > $SIZE" |bc -l) )); then
-    docker system prune -a --volumes
-else
-    break
-fi
+    # Get last character and remove it
+    last_char="`echo "${CHECK:0-1}"`"
+    CHECK="`echo ${CHECK:0:-1}`"
 
-
+    # Check size and if neccesary delete/prune docker system
+    if [ $last_char == "M" ] || [ $last_char == "K" ];then
+        echo "Its megabyte"
+        /usr/bin/sleep 3600
+        continue
+    elif [ $CHECK -ge $LIMIT ];then
+        echo "Start prune docker"
+        docker system prune -a --volumes
+        /usr/bin/sleep 3600
+    else
+        echo "Its other error"
+        break
+    fi
+done
